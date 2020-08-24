05cb7208cdd3ceb92a5acb2368f4879060992a51
comfort museo <you@example.com>
2020-08-24 20:30:48 +0300

    add inspirational quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Kwotz';
-}
+  Kwotz= [] = [
+    {id:1, name:'Be the CHANGE that you wis to see in the world- Mahtma Ghandi'},
+    {id:2, name:'The first step towards getting somewhere is to decide you are not going to stay where you are- J.P Morgan'},
+    {id:3, name:'The pessimist sees difficulty in opportunity. The optimist sees the opportunity in every difficulty- Winston Churchill'},
+    {id:4, name:'Believe you can and you are halfway there-Theodore Roosevelt'},
+    {id:5, name:'No pressure, No diamonds- Thomas Carlyle'},
+
+  ]
+  }
+
