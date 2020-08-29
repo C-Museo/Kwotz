f02d776413f0a6655549a61d654338a659df0fa3
comfort museo <you@example.com>
2020-08-29 17:54:00 +0300

    test functionality make needed changes

--- src/app/kwot/kwot.component.ts
+++ src/app/kwot/kwot.component.ts
@@ -5,17 +5,13 @@ import { kwotz } from '../kwotz';
   selector: 'app-kwot',
   templateUrl: './kwot.component.html',
   styleUrls: ['./kwot.component.css']
-})
-export class KwotComponent implements OnInit {
-  ngOnInit() {
+});
 
+quotes: kwotz [] = [
+  {id:1, name:'Be the CHANGE that you wish to see in the world- Mahtma Ghandi'},
+  {id:2, name:'The first step towards getting somewhere is to decide you are not going to stay where you are- J.P Morgan'},
+  {id:3, name:'The pessimist sees difficulty in opportunity. The optimist sees the opportunity in every difficulty- Winston Churchill'},
+  {id:4, name:'Believe you can and you are halfway there-Theodore Roosevelt'},
+  {id:5, name:'No pressure, No diamonds- Thomas Carlyle'},
 
-  quotes: kwotz [] = [
-    {id:1, name:'Be the CHANGE that you wish to see in the world- Mahtma Ghandi'},
-    {id:2, name:'The first step towards getting somewhere is to decide you are not going to stay where you are- J.P Morgan'},
-    {id:3, name:'The pessimist sees difficulty in opportunity. The optimist sees the opportunity in every difficulty- Winston Churchill'},
-    {id:4, name:'Believe you can and you are halfway there-Theodore Roosevelt'},
-    {id:5, name:'No pressure, No diamonds- Thomas Carlyle'},
-
-  ]
-  };
+ ];
--- src/app/kwotz.ts
+++ src/app/kwotz.ts
@@ -4,4 +4,11 @@ export class kwotz {
     id:number;
     name:string;
 }
-Component
+quotes: kwotz [getQuote] = [
+    {id:1, name:'Be the CHANGE that you wish to see in the world- Mahtma Ghandi'},
+    {id:2, name:'The first step towards getting somewhere is to decide you are not going to stay where you are- J.P Morgan'},
+    {id:3, name:'The pessimist sees difficulty in opportunity. The optimist sees the opportunity in every difficulty- Winston Churchill'},
+    {id:4, name:'Believe you can and you are halfway there-Theodore Roosevelt'},
+    {id:5, name:'No pressure, No diamonds- Thomas Carlyle'},
+
+  ];
